fb52ddf7a03eb7f4de6b452b736b6617ddad59a6
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-05-04 09:07:00 +0100

    At this level, I practiced the concept of module.exports, worked with different modules, imported node modules and created custom modules. Created nexted pattern modules. This helps put similar modules in a module

--- app.js
+++ app.js
@@ -2,20 +2,24 @@
 // const obj = require('./sum') //one way of accessing the private variables & functions using "obj";
 // const {x , calculateSum} = require('./sum')
 
-//ES6 Modules
-import {x, calculateSum} from './sum.js';
+const {sum, multiply} = require('./calculate');
 
+
+//ES6 Modules
+// import {x, calculateSum} from './sum.js';
+
+multiply(2,3)
 
 const courseName = "Namsta Nodejs course";
 const a = 10;
 const b = 20;
 
 // obj.calculateSum(a, b);
-calculateSum(a,b);
+// calculateSum(a,b);
 
 
 console.log(courseName);
 // console.log(obj.x)
-console.log(x)
+// console.log(x)
 // console.log(global)
 console.log(globalThis === global)
--- sum.js
+++ sum.js
@@ -1,12 +1,12 @@
 
-export const x = "Hello World";
+ const x = "Hello World";
 
-export function calculateSum(a,b){
+ function calculateSum(a,b){
     const sum = a + b;
     console.log(sum)
 }
 
-// module.exports = {
-//     x: x,
-//     calculateSum: calculateSum
-// };
+module.exports = {
+    x: x,
+    calculateSum: calculateSum
+};
